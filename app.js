be4adc556d4d30504fa2decf8a1172482b84ec24
Barr Covrigaru <barr@covrigaru.com>
2018-11-21 15:03:53 -0700

    grid is working. now working on storing colors to add to 'pixel'

--- app.js
+++ app.js
@@ -1,36 +1,17 @@
-var canvasHeight = 750;
-var canvasWidth = 750;
 var canvas = document.getElementById("canvas");
-
+var redColor = "red";
+var blueColor = "blue";
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
-  var onClick = function(event) {
-    canvas.classList.toggle("black");
+  for (var i = 0; i < 25; i++){
+    var newBox = document.createElement("div");
+    newBox.classList.add("pixel");
+
+    var onClick = function(event) {
+      event.target.classList.toggle(blueColor);
     };
-    canvas.addEventListener("click", onClick);
-    
-    for (var i = 0; i < 961; i++){
-      var newBox = document.createElement("div");
-      newBox.classList.add("pixel");
-      canvas.appendChild(newBox);
-    }
-
-    for (var i = 0; i)
+    newBox.addEventListener("click", onClick);
+    canvas.appendChild(newBox);
+  }
 });
-
-
-
-
-// var black = document.getElementById("swatch1");
-//         var red = document.getElementById("swatch2");
-//         var blue = document.getElementById("swatch3");
-//         var yellow = document.getElementById("swatch4");
-//         var green = document.getElementById("swatch5");
-//         var orange = document.getElementById("swatch6");
-//         var purple = document.getElementById("swatch7");
-//         var white = document.getElementById("swatch8");
-//         var brown = document.getElementById("swatch9");
-
-
-// canvas.addEventListener("click", onClick)
