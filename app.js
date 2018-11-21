12ce7f8dd9aa7e8936e12366dd412607f472fec1
Barr Covrigaru <barr@covrigaru.com>
2018-11-21 16:02:00 -0700

    it works

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
+var color = "white"
 var canvas = document.getElementById("canvas");
-var redColor = "red";
-var blueColor = "blue";
+var blueColors = document.getElementById("swatch3")
+blueColors.addEventListener("click", () => {
+  color = "blue"
+})
+var blackColors = document.getElementById("swatch1")
+blackColors.addEventListener("click", () => {
+  color = "black"
+})
+var yellowColors = document.getElementById("swatch4")
+yellowColors.addEventListener("click", () => {
+  color = "yellow"
+})
+var greenColors = document.getElementById("swatch5")
+greenColors.addEventListener("click", () => {
+  color = "green"
+})
+var brownColors = document.getElementById("swatch9")
+brownColors.addEventListener("click", () => {
+  color = "brown"
+})
+var orangeColors = document.getElementById("swatch6")
+orangeColors.addEventListener("click", () => {
+  color = "orange"
+})
+var purpleColors = document.getElementById("swatch7")
+purpleColors.addEventListener("click", () => {
+  color = "purple"
+})
+var whiteColors = document.getElementById("swatch8")
+whiteColors.addEventListener("click", () => {
+  color = "white"
+})
+var redColors = document.getElementById("swatch2")
+redColors.addEventListener("click", () => {
+  color = "red"
+})
+
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
@@ -9,9 +46,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
     newBox.classList.add("pixel");
 
     var onClick = function(event) {
-      event.target.classList.toggle(blueColor);
+      event.target.classList.toggle(color)
     };
+
     newBox.addEventListener("click", onClick);
     canvas.appendChild(newBox);
   }
 });
+
+
